17241fd4ded713299b8f8d9880b0809e50e07ea6
cycyjing <cycycyjing@gmail.com>
2023-04-22 09:39:28 +1200

    feat: useDebounce

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -2,35 +2,36 @@ import React,{useState,useEffect,Fragment} from 'react'
 import qs from 'qs'
 import { SearchPanel } from './SearchPanel'
 import { MainList } from './MainList'
-import { cleanObject } from 'utils'
+import { cleanObject, useDebounce, useMount } from 'utils'
 
 const apiUrl = process.env.REACT_APP_API_URL
 
 export const ProjectListScreen = () => {
-  const [users, setUsers]=useState([])
-  const [formValues,setFormValues]=useState({
+  const [users, setUsers] = useState([])
+  const [formValues,setFormValues] = useState({
     name: '',
     personId:''
   })
-  const [projectList,setProjectList]=useState([])
+  const debounceFormValues = useDebounce(formValues, 2000)
+  const [projectList,setProjectList] = useState([])
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async (response) => {
       if (response.ok) {
         console.log('response', response)
         setUsers(await response.json())
       }
     })
-  },[])
+  })
   useEffect(() => {
     // `${apiUrl}/projects?name=${formValues.projectName}&personId=${formValues.personId}`
     // it will too long if have many params, qs could help with that
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(formValues))}`).then(async (response) => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debounceFormValues))}`).then(async (response) => {
       if (response.ok) {
         setProjectList(await response.json())
       }
     })
-  },[formValues])
+  },[debounceFormValues])
   
   return (
     <Fragment>
