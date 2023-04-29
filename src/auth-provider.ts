1b6768f231d22c6a778029382ce10fd73483ca55
cycyjing <cycycyjing@gmail.com>
2023-04-29 21:27:14 +1200

    feat: achieve auth-provider

---
+++ src/auth-provider.ts
@@ -0,0 +1,59 @@
+import { User } from "prototype";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+const localStorageKey = "__auth_provider_token__";
+export const getToken = window.localStorage.getItem(localStorageKey);
+export const handleUserResponse = ({ user }: { user: User }) => {
+  window.localStorage.setItem(localStorageKey, user.token || "");
+  return user;
+};
+
+export const register = (data: { username: string; password: string }) => {
+  fetch(`${apiUrl}/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then(async (response) => {
+    if (response.ok) {
+      return handleUserResponse(await response.json());
+    } else {
+      // like throw new error
+      return Promise.reject(await response.json());
+    }
+  });
+};
+// Refactor
+export const asyncRegister = async (data: {
+  username: string;
+  password: string;
+}) => {
+  const fetchRes = await fetch(`${apiUrl}/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (fetchRes.ok) {
+    return handleUserResponse(await fetchRes.json());
+  }
+};
+
+export const login = (data: { username: string; password: string }) => {
+  fetch(`${apiUrl}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then(async (response) => {
+    if (response.ok) {
+      return handleUserResponse(await response.json());
+    }
+  });
+};
+
+export const logout = () => window.localStorage.removeItem(localStorageKey);
